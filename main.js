615c61c518e6208d0ec714bbcb6f1429d6996d9c
redihoxhaa <redi.hoxha1996@gmail.com>
2023-11-24 15:28:03 +0100

    feat: aggiunto alt

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ createApp({
     data() {
         return {
             message: 'Hello Vue!',
-            photo: 'https://picsum.photos/id/237/200/300'
+            photo: 'https://picsum.photos/id/237/200/300',
+            photo_alt: 'cane'
         }
     }
 }).mount('#app')
